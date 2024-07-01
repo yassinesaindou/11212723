fd53b5d88c2eb2ba3c4d33b90d82c291949b08f8
Yassine Saindou <yassinesaindou12@gmail.com>
2024-07-01 12:44:38 +0530

    Specification Page and integration of react router

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { HiStar } from "react-icons/hi";
 import { useParams } from "react-router";
 import styled from "styled-components";
 const H2 = styled.h2`
@@ -10,6 +11,11 @@ margin: 0 auto;
 
 `
  
+const StyledRow = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
 
 export default function ProductPage() {
     const [item, setItem] = useState();
@@ -27,8 +33,12 @@ export default function ProductPage() {
     const { productName, image, price, rating, discount, availability } = item;
     return <Styledpage>
         <H2>{productName}</H2>
-        <img src={image} alt="" width={1140}/>
-        <h3>{price} /  <span>{discount} Off</span></h3>
+        <img src={image} alt="" width={1140} />
+        <StyledRow>
+
+            <h3>{price} /  <span>{discount} Off</span></h3>
+            <p><HiStar />{ rating}</p>
+        </StyledRow>
         <div>
             <p>Description</p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati magnam, quos quia illo, incidunt eos laborum error, velit veniam quibusdam quae laboriosam molestiae vero aliquid consectetur sunt alias ullam possimus quidem. Repudiandae, quaerat beatae temporibus consequuntur rem adipisci deserunt accusamus omnis, dolorem repellat qui! Dolorem, harum? Voluptas voluptatum illum ex culpa corporis expedita perspiciatis rerum qui placeat velit, repellendus, ipsum distinctio ipsam eveniet mollitia itaque voluptates atque in, fuga repellat? Debitis quibusdam aspernatur adipisci nesciunt sed id, quod fugiat. Porro sed fuga, similique fugit corrupti laborum. Odit, perferendis placeat suscipit error consectetur labore sunt nisi aut dolore quas animi possimus.</div>
